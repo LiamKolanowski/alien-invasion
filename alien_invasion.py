195405e02a8b06b2a9dbde496874b7c5043141cd
Liam Kolanowski <LiamKolanowski@gmail.com>
2020-03-23 13:34:46 -0400

    Delete old bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,7 +31,12 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_screen()
-            self.bullets.update()       
+            self.bullets.update()
+
+            # Get rid of bullets that have disappeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)       
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
