532147d1f9c3ae77b5a05815a84a328d794ff3d4
Liam Kolanowski <LiamKolanowski@gmail.com>
2020-03-23 13:18:31 -0400

    Bullets become fireable

--- bullet.py
+++ bullet.py
@@ -1,19 +1,19 @@
 import pygame
-from pygame.sprite import sprite
+from pygame.sprite import Sprite
 
-class Bullet (Sprite):
+class Bullet(Sprite):
     """A class to manage bullets fired from the ship"""
 
     def __init__(self, ai_game):
         """Crate a bullet object a tthe ship's current position."""
         super().__init__()
-        self.screen - ai_game.screen
+        self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
 
         # Create a bullet rect at (0,0) and then set the correct position.
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
-        self.rect_midtop = ai_game.ship.rect.rect_midtop
+        self.rect.midtop = ai_game.ship.rect.midtop
 
         #Store the bullet's position as a decimal value.
         self.y = float(self.rect.y)
